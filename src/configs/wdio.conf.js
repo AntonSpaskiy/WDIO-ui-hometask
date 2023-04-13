829c68d3b4a91ab42576c0358d77184bcdeedf46
AntonS <anteydrummer@gmail.com>
2023-04-13 20:56:17 +0300

    tried appium implementation

--- src/configs/wdio.conf.js
+++ src/configs/wdio.conf.js
@@ -100,6 +100,7 @@ exports.config = {
     // If your `url` parameter starts without a scheme or `/` (like `some/path`), the base url
     // gets prepended directly.
     baseUrl: 'https://www.epam.com',
+     // default appium port
     //
     // Default timeout for all waitFor* commands.
     waitforTimeout: 5000,
@@ -110,13 +111,13 @@ exports.config = {
     //
     // Default request retries count
     connectionRetryCount: 3,
+    port: 4555,
     //
     // Test runner services
     // Services take over a specific job you don't want to take care of. They enhance
     // your test setup with almost no effort. Unlike plugins, they don't add new
     // commands. Instead, they hook themselves up into the test process.
-    services: ['chromedriver'],
-    
+    services: ['chromedriver', 'appium'],
     // Framework you want to run your specs with.
     // The following are supported: Mocha, Jasmine, and Cucumber
     // see also: https://webdriver.io/docs/frameworks
--- src/specs/smoketest.js
+++ src/specs/smoketest.js
@@ -1,4 +1,5 @@
 const {pages} = require('./../po/')
+const driver = require('appium')
 
 describe('Hometask', () => {
 
@@ -15,10 +16,12 @@ describe('Hometask', () => {
     });
 
     it('Check that the text "RESULTS FOR “AUTOMATION" is existing on search results page', async () => {
+        await driver.setClipboard('automation', 'plaintext');
         await pages('home').header.searchBtn.click();
         await pages('home').headerSearch.searchPanelOpened.waitForDisplayed();
         await pages('home').headerSearch.item('searchField').click();
-        await pages('home').headerSearch.item('searchField').addValue('automation');
+        await driver.getClipboard()
+        // await pages('home').headerSearch.item('searchField').addValue('automation');
         await pages('home').headerSearch.item('searchBtn').click();
         await expect(pages('search').searchResults.searchResultsCounter).toHaveTextContaining('result for "automation"', {ignoreCase: true});
     });
